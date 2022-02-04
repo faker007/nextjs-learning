349343f4706b31a9d3d088bcd3e9663f211c3849
faker007 <rlaqjadud195@naver.com>
2022-02-04 10:05:07 +0900

    2022-02-04 10:05 AM

---
+++ pages/events/events-search.js
@@ -0,0 +1,12 @@
+export const EventSearch = (props) => {
+    return <form>
+        <div>
+            <div>
+                <label>Year</label>
+                <select id="year">
+
+                </select>
+            </div>
+        </div>
+    </form>
+}
