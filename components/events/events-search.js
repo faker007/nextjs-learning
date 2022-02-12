17ea683cb7f205b3c4e458954fe49cf385be05de
faker007 <rlaqjadud195@naver.com>
2022-02-12 20:23:56 +0900

    nextjs/learning: 80, 81

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,41 +1,52 @@
-import Button from '../ui/button';
-import classes from './event-search/classes';
-import EventSearch from '../../components/events/events-search';
+import { useRef } from 'react';
 
-function EventSearch(props) {
+import Button from '../ui/button';
+import classes from './event-search.module.css';
+
+function EventsSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <from className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
+        <div className={classes.control}>
+          <label htmlFor="month">Month</label>
+          <select id="month" ref={monthInputRef}>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">Septemer</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </div>
       </div>
-
-      <div className={classes.control}>
-        <label htmlFor="month">Month</label>
-        <select id="month">
-          <option value="1">Junary</option>
-          <option value="2">February</option>
-          <option value="3">March</option>
-          <option value="4">April</option>
-          <option value="5">May</option>
-          <option value="6">June</option>
-          <option value="7">July</option>
-          <option value="8">Augest</option>
-          <option value="9">Septemer</option>
-          <option value="10">October</option>
-          <option value="11">November</option>
-          <option value="12">December</option>
-        </select>
-      </div>
-
       <Button>Find Events</Button>
-    </from>
+    </form>
   );
 }
 
-export default EventSearch;
+export default EventsSearch;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,24 @@
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
+  const router = useRouter();
+
   const events = getAllEvents();
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+    console.log('Hello');
+  };
 
   return (
-    <div>
+    <>
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </>
   );
 }
 
