9fa196f83c72a37aa87ae84b0facfd1c736d226f
faker007 <rlaqjadud195@naver.com>
2022-03-10 13:46:58 +0900

    nextjs learning lecture 127.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../helpers/api-utils';
 import EventList from '../components/events/event-list';
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJs Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you evlove"
+        />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
